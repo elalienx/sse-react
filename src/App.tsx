19963406be4d8956758fd6137660b284faed1ee3
Eduardo <el_alienx@hotmail.com>
2024-05-22 16:34:48 +0200

    ported the code to React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
 // Node modules
+import { useState } from "react";
+
 export default function App() {
+  // Local state
+  const [messages, setMessages] = useState<string[]>([]);
+  const [status, setStatus] = useState("On Stand by 🧊");
+
+  // Properties
+  const port = 8000;
+  let eventSource: EventSource;
+
+  // Methods
+  function onStart() {
+    setStatus("Starting connection 📡");
+    setMessages([]);
+
+    // Properties
+    eventSource = new EventSource(`http://localhost:${port}`);
+
+    eventSource.onmessage = function (event) {
+      updateMessage(event.data);
+    };
+
+    eventSource.onerror = function () {
+      endMessage();
+      eventSource.close();
+    };
+  }
+
+  function updateMessage(newMessage: string) {
+    setMessages((previousState) => [...previousState, newMessage]);
+  }
+
+  function endMessage() {
+    setStatus("Finished connection 🏁");
+  }
+
+  // Components
+  const Items = messages.map((item, index) => <li key={index}>{item}</li>);
+
   return (
     <div id="app">
       <h1>SSE React</h1>
-      <button>Start Connection 🏁</button>
+      <p>{status}</p>
+      <ol>{Items}</ol>
+      <button onClick={() => onStart()}>Start Connection 🏁</button>
     </div>
   );
 }
